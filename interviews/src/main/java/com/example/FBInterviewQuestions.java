8732cfedabfe86f062f1dd3db9f4095aca8acebd
Ilya Levin <levin@fb.com>
2019-03-07 18:25:57 -0800

    FB Interview question update

--- interviews/src/main/java/com/example/FBInterviewQuestions.java
+++ interviews/src/main/java/com/example/FBInterviewQuestions.java
@@ -1,7 +1,54 @@
 package com.example;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
 public class FBInterviewQuestions {
 
+    public static int evaluateString(String text) {
+        String[] parts = text.split("\\+");
+
+        int result = 0;
+
+        for (String part : parts) {
+            if (part.contains("*")) {
+                String[] productParts = part.split("\\*");
+
+                int productResult = 1;
+                for (String productPart : productParts) {
+                    productResult *= Integer.decode(productPart);
+                }
+                result += productResult;
+            } else {
+                result += Integer.decode(part);
+            }
+        }
+
+        return result;
+    }
+
+    public static String removeBrackets(String input) {
+
+        int openBracketCounter = 0;
+        StringBuilder sb = new StringBuilder();
+         for (char ch : input.toCharArray()) {
+             if (ch == '(') {
+                 openBracketCounter++;
+                 sb.append(ch);
+             } else {
+                 if (openBracketCounter > 0) {
+                     openBracketCounter--;
+                     sb.append(ch);
+                 }
+             }
+         }
+
+         if (sb.length() <= 1) { return ""; }
+
+         return sb.toString();
+    }
 
     public static boolean isValidNumber(String number) {
 
@@ -41,5 +88,122 @@ public class FBInterviewQuestions {
         }
 
         return wasDigitFound;
+    }
+
+    public static class RandomCity {
+
+        private final String[] mCities;
+        private final int[] mRunningPopulationSum;
+
+        private Random mRandom;
+        private int mTotalPopulation;
+
+
+        public RandomCity(Map<String, Integer> citiesToPopulationMap) {
+            mCities = new String[citiesToPopulationMap.size()];
+            mRunningPopulationSum = new int[citiesToPopulationMap.size()];
+
+            mRandom = new Random();
+
+            int index = 0;
+            mTotalPopulation = 0;
+            for (Map.Entry<String, Integer> entry : citiesToPopulationMap.entrySet()) {
+                mCities[index] = entry.getKey();
+                mRunningPopulationSum[index] = mTotalPopulation + entry.getValue();
+                mTotalPopulation += entry.getValue();
+                index++;
+            }
+        }
+
+        public String getRandomCity() {
+            int random = mRandom.nextInt(mTotalPopulation);
+            return mCities[findIntervalPosition(random, mRunningPopulationSum)];
+        }
+
+        public int findIntervalPosition(int val, int[] arr) {
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start < end) {
+                int medium = (start + end) / 2;
+
+                if (val > arr[medium]) {
+                    start = medium + 1;
+                    continue;
+                }
+
+                if (medium >= 1) {
+                    if (val < arr[medium] && val > arr[medium - 1]) {
+                        return medium;
+                    }
+                    end = medium - 1;
+                    continue;
+                } else {
+                    return 0;
+                }
+            }
+
+            return start;
+        }
+    }
+
+    public static class RandomDataStorage {
+
+        private ArrayList<Integer> mStorage;
+        private Random mRandom;
+
+        public RandomDataStorage() {
+            mStorage = new ArrayList<>();
+            mRandom = new Random();
+        }
+
+        public void add(int value) {
+            mStorage.add(value);
+        }
+
+        public int getRandom() {
+            int index = mRandom.nextInt(mStorage.size());
+            int temp = mStorage.get(index);
+            mStorage.set(index, mStorage.get(mStorage.size() - 1));
+            mStorage.remove(mStorage.size() - 1);
+            return temp;
+        }
+    }
+
+
+    public static long[] getArrayOfProducts(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        int zeroCounter = 0;
+        int zeroPosition = -1;
+        long totalProduct = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                if (zeroCounter == 0) {
+                    zeroCounter++;
+                    zeroPosition = i;
+                    continue;
+                } else {
+                    return new long[arr.length];
+                }
+            }
+            totalProduct *= arr[i];
+        }
+
+        long[] result = new long[arr.length];
+        if (zeroCounter == 1) {
+
+            result[zeroPosition] = totalProduct;
+            return result;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = totalProduct / arr[i];
+        }
+
+        return result;
     }
 }
--- interviews/src/test/java/com/example/FBInterviewQuestionsTest.java
+++ interviews/src/test/java/com/example/FBInterviewQuestionsTest.java
@@ -3,10 +3,33 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 import static com.example.FBInterviewQuestions.*;
 
 public class FBInterviewQuestionsTest {
+
+    @Test
+    public void testEvaluateString() {
+
+        System.out.println("1+2+3 = " + evaluateString("1+2+3"));
+        System.out.println("1*2+3 = " + evaluateString("1*2+3"));
+        System.out.println("1*2*3 = " + evaluateString("1*2*3"));
+        System.out.println("10*2*3+4+5*6 = " + evaluateString("10*2*3+4+5*6"));
+    }
+
+    @Test
+    public void testRemoveBrackets() {
+        System.out.println("(())()()( -> " + removeBrackets("(())()()("));
+        System.out.println(")()(((()())() -> " + removeBrackets(")()(((()())()"));
+        System.out.println("((((( -> " + removeBrackets("((((("));
+        System.out.println("))))))) -> " + removeBrackets(")))))))"));
+        System.out.println("( -> " + removeBrackets("("));
+        System.out.println(") -> " + removeBrackets(")"));
+    }
 
     @Test
     public void testIsValidNumber() {
@@ -23,5 +46,67 @@ public class FBInterviewQuestionsTest {
         Assert.assertFalse(isValidNumber("-.."));
         Assert.assertFalse(isValidNumber("123 343"));
         Assert.assertFalse(isValidNumber("3434_343"));
+    }
+
+    @Test
+    public void testRandomCity() {
+
+        Map<String, Integer> cities = new HashMap<>();
+        cities.put("NY", 600);
+        cities.put("London", 300);
+        cities.put("SF", 100);
+        cities.put("SPb", 100);
+        cities.put("Moscow", 500);
+        cities.put("Singapore", 400);
+
+
+        RandomCity randomCity = new RandomCity(cities);
+
+        int numberOfNY = 0;
+        int numberOfLondon = 0;
+        int numberOfSF = 0;
+        int numberOfSPb = 0;
+        int numberOfMoscow = 0;
+        int numberOfSingapore = 0;
+
+        for (int i = 0; i < 10000; i++) {
+            String city = randomCity.getRandomCity();
+            if (city.equals("NY")) numberOfNY++;
+            else if (city.equals("London")) numberOfLondon++;
+            else if (city.equals("SF")) numberOfSF++;
+            else if (city.equals("SPb")) numberOfSPb++;
+            else if (city.equals("Moscow")) numberOfMoscow++;
+            else if (city.equals("Singapore")) numberOfSingapore++;
+        }
+
+        System.out.println("NY = " + numberOfNY
+                + ", London = " + numberOfLondon
+                + ", SF = " + numberOfSF
+                + ", SPb = " + numberOfSPb
+                + ", Moscow = " + numberOfMoscow
+                + ", Singapore = " + numberOfSingapore
+        );
+
+    }
+
+    @Test
+    public void testRandomDataStorage() {
+        RandomDataStorage randomDataStorage = new RandomDataStorage();
+        for (int  i = 0; i < 100; i++) {
+            randomDataStorage.add(i + 1);
+        }
+
+        for (int  i = 1; i <= 100; i++) {
+            System.out.print("" + randomDataStorage.getRandom() + " ");
+            if (i% 10 == 0) {System.out.println();}
+        }
+    }
+
+    @Test
+    public void testGetArrayOfProducts() {
+        System.out.println(Arrays.toString(getArrayOfProducts(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(getArrayOfProducts(new int[]{1, 0, 3})));
+        System.out.println(Arrays.toString(getArrayOfProducts(new int[]{1, 0, 3, 0})));
+        System.out.println(Arrays.toString(getArrayOfProducts(new int[]{3, 4, 3, 9, 8 ,6 , 10})));
     }
 }
